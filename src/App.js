fe6bb7e31d3d3dfd1b35c3605583ea0172811a44
Rhys <Rhys26@hotmail.com>
2019-02-03 14:35:32 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Grid, AppBar, Typography} from '@material-ui/core'
+import Carosel from './Components/carosel';
+import Info from './Components/info'
+
+const style = {
+  margin: '20px'
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Grid container align="center">
+          <Grid item xs={12}>
+            <AppBar position="static" id="appbar">
+              <Typography variant="headline" align="center" color="inherit" style={style}>
+                The Nine
+              </Typography>
+            </AppBar>
+          </Grid>
+          <Grid item xs={12} align="center" className="m">
+            <Carosel />
+          </Grid>
+          <Grid item xs={12}>
+            <Info TitleOne="Something" TitleTwo="Something Else" />
+          </Grid>
+        </Grid>
       </div>
     );
   }
